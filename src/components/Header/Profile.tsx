1dbc3c30ff51854af56e16cbe4210621b920bdf3
ArmandoMarioto <armando_marioto100@hotmail.com>
2022-07-21 12:56:07 -0300

    Header responsivo

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,17 +1,23 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
-
-export function Profile(){
-    return(
-        <Flex align='center'>
-            <Box mr='4' textAlign='right'>
-                <Text>
-                    Armando Marioto
-                </Text>
-                <Text fontSize='small' color='gray.300'>
-                    armandomarioto@gmail.com
-                </Text>
-            </Box>
-            <Avatar size='md' name='Armando Marioto'src='https://github.com/armandomarioto.png'/>
-        </Flex>
-    );
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+export function Profile({ showProfileData = true }: ProfileProps) {
+  return (
+    <Flex align="center">
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Armando Marioto</Text>
+          <Text fontSize="small" color="gray.300">
+            armandomarioto@gmail.com
+          </Text>
+        </Box>
+      )}
+      <Avatar
+        size="md"
+        name="Armando Marioto"
+        src="https://github.com/armandomarioto.png"
+      />
+    </Flex>
+  );
 }
