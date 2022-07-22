ceac3e1b16e644eab89edbf9e6891cf7967ee0eb
ArmandoMarioto <armando_marioto100@hotmail.com>
2022-07-21 23:06:57 -0300

    Atualizando react hook form e aplicando no login

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,26 +1,38 @@
-import { FormControl, FormLabel, Input as ChakraInput, InputProps as ChakraInputProps } from "@chakra-ui/react";
+import {
+  FormControl,
+  FormLabel,
+  Input as ChakraInput,
+  InputProps as ChakraInputProps,
+} from "@chakra-ui/react";
+import { forwardRef, ForwardRefRenderFunction } from "react";
 
-interface InputProps extends ChakraInputProps{
-    name: string;
-    label?: string;
+interface InputProps extends ChakraInputProps {
+  name: string;
+  label?: string;
 }
 
-export function Input({name, label, ...rest}: InputProps ){
-    return (
-        <FormControl>
-        {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
-        <ChakraInput
-          id={name}
-          name={name}
-          focusBorderColor="pink.500"
-          bg="gray.900"
-          variant="filled"
-          _hover={{
-            bgColor: "gray.900",
-          }}
-          size="lg"
-          {...rest}
-        />
-        </FormControl>
-    );
-}
+export const InputBase: ForwardRefRenderFunction<
+  HTMLInputElement,
+  InputProps
+> = ({ name, label, ...rest }, ref) => {
+  return (
+    <FormControl>
+      {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
+      <ChakraInput
+        id={name}
+        name={name}
+        focusBorderColor="pink.500"
+        bg="gray.900"
+        variant="filled"
+        _hover={{
+          bgColor: "gray.900",
+        }}
+        size="lg"
+        ref={ref}
+        {...rest}
+      />
+    </FormControl>
+  );
+};
+
+export const Input = forwardRef(InputBase);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,22 @@
 import { Button, Flex, Stack } from "@chakra-ui/react";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { Input } from "../components/Form/Input";
+
+type SignInFormData = {
+  email: string;
+  password: string;
+}
 export default function SighIn() {
+  const { register, handleSubmit, formState } = useForm();
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async (values)  => {
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    
+    console.log(values);
+
+    
+  }
+
   return (
     <Flex w="100vw" h="100vh" align="center" justify="center">
       <Flex
@@ -11,15 +27,17 @@ export default function SighIn() {
         p="8"
         borderRadius={8}
         flexDir="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing="4">
-          <Input name="email" label="E-mail" type="email" />
-          <Input name="password" label="Senha" type="password" />
+          <Input name="email" label="E-mail" type="email" {...register('email')}/>
+          <Input name="password" label="Senha" type="password" {...register('password')}/>
         </Stack>
-        <Button type="submit" mt="6" colorScheme={"pink"} size="lg">
+        <Button type="submit" mt="6" colorScheme={"pink"} size="lg" isLoading={formState.isSubmitting}>
           Entrar
         </Button>
       </Flex>
     </Flex>
   );
 }
+
